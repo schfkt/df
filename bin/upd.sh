dcd59827a5b9abe2f84232744ed1d799ffea7529
Pavel Ivanov <ivpavig@gmail.com>
2023-06-27 10:02:35 +0300

    Fix upd script: vim -> nvim

--- bin/upd.sh
+++ bin/upd.sh
@@ -2,17 +2,15 @@
 
 echo "Updating packages..."
 if [[ $OSTYPE =~ ^darwin ]]; then
-  brew update
-  brew upgrade
-  brew bundle
+	brew update
+	brew upgrade
+	brew bundle
 elif [[ $OSTYPE =~ ^linux ]]; then
-  sudo apt update
-  sudo apt upgrade -y
+	sudo apt update
+	sudo apt upgrade -y
 fi
 
-echo "Updating VIM and it's plugins..."
-vim +PlugUpdate +PlugUpgrade +qall
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer --go-completer --ts-completer --rust-completer
+echo "Updating neovim and it's plugins..."
+nvim +PlugUpdate +PlugUpgrade +qall
 
 echo "Done!"
